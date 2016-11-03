44639b8f76ff700137917c43642f6f3383fa1f1d
orghoN <anoronyo@gmail.com>
2016-11-03 17:27:40 -0500

    finished mult

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,20 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R2 //set output to 0
+M=0
+(START) //begin loop
+@R1
+M=M-1  // R1=R1-1
+D=M
+@END
+D;JLT //End Loop if R1<0
+@R0
+D=M    // D=R0
+@R2
+M=D+M //R2=R2+R1
+@START
+0;JMP //Go back to beginning of loop
+(END)
+@END
+0;JMP  // Infinite loop
